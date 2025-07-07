f7e62831e5071b05f04e3b9f7a8809eed02ac1ce
Garjelin <sergeyyakimov89@gmail.com>
2025-07-07 07:42:05 +0500

    2025-07-07_1

--- app/src/main/java/com/example/todo_list/MainActivity.kt
+++ app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.todo_list
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,17 +17,36 @@ import com.example.todo_list.ui.theme.TODO_LISTTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        Log.d("MainActivity", "onCreate called")
         setContent {
             TODO_LISTTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting("Android")
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy called")
     }
 }
 
